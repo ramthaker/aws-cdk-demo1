cb244cec84a881cafd98a6cdb28f327d5217720e
Ram Thaker <ramthaker123@gmail.com>
2025-11-21 17:57:26 +0530

    remove extra lambda

--- lambda/items.js
+++ lambda/items.js
@@ -3,7 +3,6 @@
 exports.handler = async (event) => {
   console.log('Event:', JSON.stringify(event));
   return {
-    
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello from Lambda', event }),
   };
